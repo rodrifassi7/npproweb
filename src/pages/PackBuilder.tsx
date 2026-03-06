d7e7d8f0597864d61690ecad75b88099630bae0d
Rodrigo <rodrifassi7@gmail.com>
2026-03-06 10:46:07 -0300

    Fix remaining TS errors in PackBuilder

--- src/pages/PackBuilder.tsx
+++ src/pages/PackBuilder.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useCart } from '../context/CartContext';
 import { MENU, CONFIG, PREMADE_PACKS } from '../data/data';
 import { Minus, Plus, ShoppingCart, Info, ShieldCheck, Flame, Scale, Send, Sparkles, Truck } from 'lucide-react';
